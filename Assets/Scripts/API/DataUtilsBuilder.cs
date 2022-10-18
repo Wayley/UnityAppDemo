18e6777c018afd82d21bcd64270fa7d3d6c43b08
zheng.wang <zheng.Wang@mobiledrivetech.com>
2022-10-18 10:57:37 +0800

    add aar file

--- Assets/Scripts/API/DataUtilsBuilder.cs
+++ Assets/Scripts/API/DataUtilsBuilder.cs
@@ -2,19 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataUtilsBuilder
-{
-    DataUtils DataUtils = null;
-    DataUtils createUtils()
-    {
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            return new AndroidDataUtils();
-        }
-        return DataUtils;
-    }
-}
-public class AndroidDataUtils : MonoBehaviour, DataUtils
+public class AndroidDataUtils : MonoBehaviour
 {
     static AndroidJavaObject _ajc;
 
@@ -23,14 +11,27 @@ public class AndroidDataUtils : MonoBehaviour, DataUtils
         _ajc = new AndroidJavaObject("com.mobiledrivetech.smartclock.Utils");
     }
 
-
-    public string GetWifiName()
+    //获取底层指南针的数据
+    public static int GetCompassData()
     {
-        return "MY_WIFI_NAME";
+        Debug.Log("start getCompassData......");
+        string data = _ajc.Call<string>("getCompass");
+        Debug.Log("getCompassData, data: " + data);
+        return !string.IsNullOrEmpty(data) ? int.Parse(data) : 0;
     }
-    public bool GetWifiStatus()
-    {
-        return true;
 
+    public static void setMenu(string menu)
+    {
+        Debug.Log("setMenu, menu: " + menu);
+        _ajc.Call("setMenu", menu);
+        Debug.Log("setMenu ok");
     }
+
+    public static void setClockTheme(string theme)
+    {
+        Debug.Log("setClockTheme, theme: " + theme);
+        _ajc.Call("setClockTheme", theme);
+        Debug.Log("setClockTheme ok");
+    }
+
 }
